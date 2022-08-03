53216409d4c635a9e49ca062e3ead8ee3be920b3
unknown <emmysoetan@gmail.com>
2022-08-03 21:38:44 +0100

    Hooked up tailwind css to the project

---
+++ client/src/logic/firebase.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCjmybbVESslSwNnjvurTV4Xv7ce7-JXkU",
+  authDomain: "linkedin-clone-5f1f8.firebaseapp.com",
+  projectId: "linkedin-clone-5f1f8",
+  storageBucket: "linkedin-clone-5f1f8.appspot.com",
+  messagingSenderId: "1088763937942",
+  appId: "1:1088763937942:web:e9d325d472972bbac16eb7"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
